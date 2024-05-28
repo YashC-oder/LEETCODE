f67efcfe6f36998e96a4b71538bab8b824c0ff68
YashC-oder <yashmehra914@gmail.com>
2024-05-28 19:11:34 +0530

    38.countAndSay

---
+++ countAndSay.cpp
@@ -0,0 +1,24 @@
+class Solution {
+string rle(string str){
+    
+    string res = "";
+    char previous = str[0];
+    int count = 1;
+    for(int i = 1;i<str.size();i++){
+        if(str[i] == previous){
+            count++;
+        }else{
+            res += to_string(count) + previous;
+            previous = str[i];
+            count = 1;
+        }
+    }
+    return res + to_string(count) + previous;
+}
+public:
+    string countAndSay(int n) {
+        if(n == 1) return "1";
+
+        return rle(countAndSay(n-1));
+    }
+};
