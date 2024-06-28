45a0ed2137caa0a59faad05f400776226e746f41
YashC-oder <yashmehra914@gmail.com>
2024-06-28 12:23:45 +0530

    BSTIterator

---
+++ BSTIterator.cpp
@@ -0,0 +1,69 @@
+class BSTIterator {
+private:
+    TreeNode* FlattenBST(TreeNode* root){
+        if(!root) return nullptr;
+        TreeNode* node = new TreeNode(root->val);
+        TreeNode* left = FlattenBST(root->left);
+        node->right = FlattenBST(root->right);
+
+        while(left && left->right){
+            left = left->right;
+        }
+        node->left = left;
+        if(node->left) node->left->right = node;
+        if(node->right) node->right->left = node;
+
+        while(node && node->left){
+            node = node->left;
+        }
+        return node;
+    }
+
+    TreeNode* head;
+    TreeNode* current;
+public:
+    BSTIterator(TreeNode* root) {
+        head = new TreeNode(0);
+        head->right = FlattenBST(root);
+        current = head;
+    }
+    
+    int next() {
+        current = current->right;
+        return current->val;
+    }
+    
+    bool hasNext() {
+        return current->right;
+    }
+};
+
+/*-----------------------------------------------------------------------------------------*/
+
+class BSTIterator {
+private:
+    vector<int> inorder;
+    int current = 0;
+    int size;
+    void traverse(TreeNode* root, vector<int> &inorder) {
+        if(!root) return;
+        traverse(root -> left, inorder);
+        inorder.push_back(root -> val);
+        traverse(root -> right, inorder);
+    }
+public:
+    BSTIterator(TreeNode* root) {
+        traverse(root, inorder);
+        inorder.insert(inorder.begin() ,0);
+        size = inorder.size();
+    }
+    
+    int next() {
+        return inorder[++current];
+    }
+    
+    bool hasNext() {
+        if(current < size) return true;
+        else return false;
+    }
+};
