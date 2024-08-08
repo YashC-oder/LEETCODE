d2a3d44ef49bc57719c213fad21cc1aad35c2f95
YashC-oder <yashmehra914@gmail.com>
2024-08-08 12:13:21 +0530

    .

--- spiralMatrixIII.cpp
+++ spiralMatrixIII.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         int size = rows * cols;
         int row = rStart , col = cStart;
-        int L = 1 , R = 1 ;
+        int L = 1 ;
         bool direction = true;
         vector<vector<int>> indexes;
 
@@ -16,14 +16,14 @@ public:
                 }
             }
 
-            for(int i = 0;i<R;i++){
+            for(int i = 0;i<L;i++){
                 row += direction ? 1 : -1;
                 if(row < rows && col < cols && row >= 0 && col >=0){
                     indexes.push_back({row,col});
                 }
             }
 
-            L++,R++;
+            L++;
             direction = !direction;
         }
 
