4808105897336008efdc1410c80892c4e0cb334d
YashC-oder <yashmehra914@gmail.com>
2024-08-13 11:48:38 +0530

    optimisation

--- increasingTriplet.cpp
+++ increasingTriplet.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int num1 = INT_MAX;
         int num2 = INT_MAX;
-
         for (const int& num : nums) {
             if (num <= num1) {
                 num1 = num;
